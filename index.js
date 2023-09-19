5f2a7c8c6fff2f3a02224f008f376453c99472a1
Nijanshu <nijanshusingh@gmail.com>
2023-09-19 21:00:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,46 @@
-
 require('dotenv').config();
 
-const connectToMongo= require('./db');
+const connectToMongo = require('./db');
 
-// index.js
 const express = require('express');
-// const connectToMongo = require('./db');
-
-const BASE_URL = process.env.BASE_URL||4000;
-const mon= process.env.mongoDBURI
+const BASE_URL = process.env.BASE_URL || 4000;
+const mon = process.env.mongoDBURI;
 
 const app = express();
-const port = 400;
 
-var cors = require('cors')
-app.use(cors({
+var cors = require('cors');
+app.use(
+  cors({
     origin: 'https://notes-sigma-ruddy.vercel.app/login',
     methods: ['POST', 'GET'],
-    credentials: true
-}))
-app.use(express.json())
+    credentials: true,
+  })
+);
 
-
-app.use(express.json());    
+app.use(express.json());
 
 (async () => {
-    // Connect to MongoDB before starting the Express app
-    await connectToMongo();
+  // Connect to MongoDB before starting the Express app
+  await connectToMongo();
 
-    // Define your routes and middleware here
-    // app.get('/', (req, res) => {
-    //     res.send('Hello Harry');
-    // });
+  // Available routes
+  app.use('/api/auth', require('./routes/auth'));
+  app.use('/api/notes', require('./routes/notes'));
 
+  app.get('/', (req, res) => {
+    res.json('hello');
+  });
 
-    //Available routes
-    app.use('/api/auth', require('./routes/auth'));
-    app.use('/api/notes', require('./routes/notes'));
-
-app.get('/', (req, res) => {
-    res.json("hello")
-})
-
-//For Heroku deployment
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static("client/build"));
-
-}
-
-
-    // Start the Express app
-    app.listen(port, () => {
-        console.log(`iBlog listening at ${BASE_URL}`);
+  // For Heroku deployment
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('/', (req, res) => {
+      res.json('hello');
     });
+  }
+
+  // Start the Express app
+  app.listen(() => {
+    console.log(`iBlog listening at ${BASE_URL}`);
+  });
 })();
