87947ba8508fe21ed658ac7ad26c1169daad95a7
nijjm <145488614+nijjm@users.noreply.github.com>
2023-09-19 23:47:20 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,25 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const mongoURI=process.env.mongoDBURI;
+// Retrieve the MongoDB URI from environment variables
+const mongoURI = process.env.mongoDBURI;
 
-
+// Define a function to connect to MongoDB
 const connectToMongo = async () => {
-    try {
-        await mongoose.connect(mongoURI);
-        console.log(`Connected to MongoDB Successfully`);
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        // Optionally, you can throw the error to stop the application
-        // throw error;
+  try {
+    // Check if there's an existing connection before connecting
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log(`Connected to MongoDB Successfully`);
     }
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    // Optionally, you can throw the error to stop the application
+    // throw error;
+  }
 };
 
 module.exports = connectToMongo;
